12cfac205c18deb6e61148fdb12af1b59d5a116c
Valdir Ronis <valdirronis@gmail.com>
2025-03-30 08:29:36 -0300

    Update personalInfo.ts

--- src/data/personalInfo.ts
+++ src/data/personalInfo.ts
@@ -23,7 +23,7 @@ export const personalInfo: PersonalInfo = {
     state: "Ceará",
     country: "Brasil",
   },
-  avatarUrl: "https://github.com/x1db.png?size=200",
+  avatarUrl: "https://github.com/xogm.png?size=200",
   backgroundUrl:
     "https://images.pexels.com/photos/5926397/pexels-photo-5926397.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   bio: "Apaixonado por tecnologia e inovação. Sempre buscando novas oportunidades para aplicar minhas habilidades e contribuir para o sucesso de projetos desafiadores.",
